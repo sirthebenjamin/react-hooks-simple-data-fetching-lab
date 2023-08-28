51e63b2ea54af8cf5b3c5c0b803eb31e511e997f
sirthebenjamin <126521851+sirthebenjamin@users.noreply.github.com>
2023-08-27 22:43:37 -0400

    Adds myfile.md

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,44 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [dogImage, setDogImage] = useState(null);
+
+  useEffect(() => {
+    async function fetchDogImage() {
+      try {
+        const response = await fetch("https://dog.ceo/api/breeds/image/random");
+        if (response.ok) {
+          const data = await response.json();
+          setDogImage(data.message);
+          setLoading(false);
+        } else {
+          throw new Error("Failed to fetch dog image");
+        }
+      } catch (error) {
+        console.error(error);
+        setLoading(false);
+      }
+    }
+
+    fetchDogImage();
+  }, []);
+
+  return (
+    <div className="App">
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <img src={dogImage} alt="A Random Dog" />
+      )}
+    </div>
+  );
+}
+
+export default App;
+ 
+
+
+
+
+
